3cffac8de1eddea90fd3d7b0e9b0ebf52cf75f18
andymuncey <andrewmuncey@Andrews-MacBook-Pro.local>
2015-07-21 13:45:39 +0100

    Initial Commit

---
+++ Bowling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Bowling
+//
+//  Created by Andrew Muncey on 21/07/2015.
+//  Copyright (c) 2015 University of Chester. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
