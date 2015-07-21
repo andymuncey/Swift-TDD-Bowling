650dea465c8a07a510bf5a1d4935f65371005498
andymuncey <andrewmuncey@Andrews-MacBook-Pro.local>
2015-07-21 15:13:18 +0100

    Added a few more tests

--- BowlingTests/BowlingTests.swift
+++ BowlingTests/BowlingTests.swift
@@ -20,8 +20,39 @@ class BowlingTests: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
+        scorer = nil
     }
     
+    
+    func testStrike(){
+        scorer.addThrow(10)
+        scorer.addThrow(2)
+        scorer.addThrow(1)
+        var frame1Score = scorer.scoreForFrameIndex(0)
+        XCTAssert(frame1Score == 13,"Strike test");
+    }
+    
+    func testSpare(){
+        //ball 1
+        scorer.addThrow(5)
+        scorer.addThrow(5) //10 for frame 2, 14 with next ball added on
+        scorer.addThrow(6)
+        scorer.addThrow(1)
+        var frame1Score = scorer.scoreForFrameIndex(0)
+        XCTAssert(frame1Score == 16,"Two Spares test, value was \(frame1Score), should be 16")
+    }
+    
+    func testTwoSpares(){
+        //ball 1
+        scorer.addThrow(2)
+        scorer.addThrow(4) //6 for frame 1
+        scorer.addThrow(5)
+        scorer.addThrow(5) //10 for frame 2, 14 with next ball added on
+        scorer.addThrow(4)
+        scorer.addThrow(6)
+        var frame2Score = scorer.scoreForFrameIndex(1)
+        XCTAssert(frame2Score == 14,"Two Spares test, value was \(frame2Score), should be 14")
+    }
     
     func testTurkey(){
         
@@ -39,95 +70,69 @@ class BowlingTests: XCTestCase {
         XCTAssert(frame1Score == 0,"Double gutterball OK");
     }
     
-    func testTwoSpares(){
-        //ball 1
+    func testSampleGame(){
         scorer.addThrow(2)
-        scorer.addThrow(4) //6 for frame 1
-        scorer.addThrow(5)
-        scorer.addThrow(5) //10 for frame 2, 14 with next ball added on
         scorer.addThrow(4)
+        scorer.addThrow(4)
+        scorer.addThrow(5)
         scorer.addThrow(6)
-        var frame1Score = scorer.scoreForFrameIndex(1)
-        XCTAssert(frame1Score == 14,"Two Spares test, value was \(frame1Score), should be 14")
+        scorer.addThrow(4)
+        scorer.addThrow(5)
+        scorer.addThrow(5)
+        scorer.addThrow(10)
+        scorer.addThrow(4)
+        scorer.addThrow(1)
+        scorer.addThrow(7)
+        scorer.addThrow(3)
+        scorer.addThrow(6)
+        scorer.addThrow(4)
+        scorer.addThrow(10)
+        scorer.addThrow(2)
+        scorer.addThrow(8)
+        scorer.addThrow(6)
+        var totalScore = scorer.totalScore()
+        XCTAssert(totalScore == 142,"Sample Game (142), value was \(totalScore)")
     }
     
-    func testLastFramePerfectGame(){
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)//10th frame
-        scorer.addThrow(10)
-        scorer.addThrow(10)
+    func testLastFrameOfPerfectGame(){
+        for i in 1...13{
+            scorer.addThrow(10)
+        }
         var frame10Score = scorer.scoreForFrameIndex(9)
         XCTAssert(frame10Score == 30,"Last Frame Perfect Game, value was \(frame10Score), should be 30")
     }
     
-    func test9thFramePerfectGame(){
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)//10th frame
-        scorer.addThrow(10)
-        scorer.addThrow(10)
+    func test9thFrameOfPerfectGame(){
+        for i in 1...13{
+            scorer.addThrow(10)
+        }
         var frame9Score = scorer.scoreForFrameIndex(8)
         XCTAssert(frame9Score == 30,"9th Perfect Game failed, value was \(frame9Score), should be 30")
     }
     
     func testPerfectGame(){
+        
+        for i in 1...13{
         scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)//10th frame
-        scorer.addThrow(10)
-        scorer.addThrow(10)
+        }
+
         var totalScore = scorer.totalScore()
         XCTAssert(totalScore == 300,"Perfect Game Failed, value was \(totalScore)")
     }
     
     func testImperfectGame(){
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)
-        scorer.addThrow(10)//10th frame
-        scorer.addThrow(10)
+        for i in 1...11{
+            scorer.addThrow(10)
+        }
         scorer.addThrow(9)
         var totalScore = scorer.totalScore()
         XCTAssert(totalScore == 299,"Imperfect Game Failed, value was \(totalScore)")
     }
     
     func spare9thFrame(){
-        scorer.addThrow(10)//30
-        scorer.addThrow(10)//60
-        scorer.addThrow(10)//90
-        scorer.addThrow(10)//120
-        scorer.addThrow(10)//150
-        scorer.addThrow(10)//180
-        scorer.addThrow(10)//205
-        scorer.addThrow(10)//225
+        for i in 1...8{
+            scorer.addThrow(10)
+        }
         scorer.addThrow(5)
         scorer.addThrow(5)//245
         scorer.addThrow(10)//10th frame
